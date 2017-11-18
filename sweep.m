37eba7dceedd83d0120892d1656b9e1195f0c1f4
ammar903@gmail.com <ammar903@gmail.com>
2017-11-18 10:11:33 +0000

    first commit

---
+++ sweep.m
@@ -0,0 +1,19 @@
+gain=[1:8];
+control_lab_1;
+
+ for x=1:length(gain)
+  K=gain(x);
+  fname = ['K_' num2str(x) '.mat'];
+  set_param('control_lab_1/output','Filename',fname);
+  
+  sim('control_lab_1');
+  
+  load(fname);
+  plot(ans);
+  title(['Response of system to Step input with K =' num2str(x)]);
+  
+  sname = ['K_' num2str(x) '.png'];
+  saveas(gcf, sname);
+  
+
+ end
